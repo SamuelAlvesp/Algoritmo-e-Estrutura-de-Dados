bfe4040e8eeec84c1e7e7bdb3df73da6e0fe6302
SamuelAlvesp <samueka18@gmail.com>
2024-08-16 19:22:20 -0300

    questao6.c

--- questao6.c
+++ questao6.c
@@ -7,8 +7,8 @@ int main()
     scanf ("%f", &horas);
     printf ("Digite a velocidade media em KM/H \n");
     scanf ("%f", &velocidade);
-    distancia= velocidade/horas;
-    litros= distancia*12;
+    distancia= velocidade*horas;
+    litros= distancia/12;
     
     printf ("A quantidade utilizada de gasolina foi %.0fL, e a distancia percorrida %.0fkm" , litros,distancia);
     return 0;
